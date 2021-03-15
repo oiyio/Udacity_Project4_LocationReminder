e971d8221f219479c44e2b06023c9da7b8249bbe
OMERIYIOZ <omer.iyioz@turkcell.com.tr>
2021-03-15 13:23:57 +0300

    added login feature.

--- starter/app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ starter/app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -1,8 +1,19 @@
 package com.udacity.project4.authentication
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import com.udacity.project4.R
+import androidx.lifecycle.Observer
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+import com.udacity.project4.databinding.ActivityAuthenticationBinding
+import com.udacity.project4.locationreminders.RemindersActivity
+import java.util.*
 
 /**
  * This class should be the starting point of the app, It asks the users to sign in / register, and redirects the
@@ -10,15 +21,76 @@ import com.udacity.project4.R
  */
 class AuthenticationActivity : AppCompatActivity() {
 
+    private val viewModel by viewModels<AuthenticationViewModel>()
+
+    private lateinit var binding: ActivityAuthenticationBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_authentication)
-//         TODO: Implement the create account and sign in using FirebaseUI, use sign in using email and sign in using Google
 
-//          TODO: If the user was authenticated, send him to RemindersActivity
+        binding = ActivityAuthenticationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonLogin.setOnClickListener {
+            launchSignInFlow()
+        }
+
+
+        viewModel.authenticationState.observe(this, Observer {
+            when (it) {
+                AuthenticationViewModel.AuthenticationState.AUTHENTICATED -> {
+                    val intent = Intent(this@AuthenticationActivity, RemindersActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+                AuthenticationViewModel.AuthenticationState.UNAUTHENTICATED -> {
+                    Toast.makeText(this, "unauthenticated !", Toast.LENGTH_LONG).show()
+                }
+                else -> {
+                    Log.e(TAG, "Authentication state that doesn't require any UI change $it")
+                }
+            }
+        })
 
 //          TODO: a bonus is to customize the sign in flow to look nice using :
         //https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
 
+    }
+
+    private fun launchSignInFlow() {
+        // Give users the option to sign in / register with their email or Google account. If users
+        // choose to register with their email, they will need to create a password as well.
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(), AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+
+        // Create and launch sign-in intent. We listen to the response of this activity with the
+        // SIGN_IN_RESULT_CODE code.
+        startActivityForResult(
+            AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(
+                providers
+            ).build(), SIGN_IN_RESULT_CODE
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == SIGN_IN_RESULT_CODE) {
+            val response = IdpResponse.fromResultIntent(data)
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in user. viewModel.authenticationState is changed automatically and opens RemindersActivity
+                Log.i(TAG, "Successfully signed in user " + "${FirebaseAuth.getInstance().currentUser?.displayName}!")
+            } else {
+                // Sign in failed. If response is null the user canceled the sign-in flow using
+                // the back button. Otherwise check response.getError().getErrorCode() and handle
+                // the error.
+                Log.i(TAG, "Sign in unsuccessful ${response?.error?.errorCode}")
+            }
+        }
+    }
+
+    companion object{
+        val TAG: String = AuthenticationActivity::class.java.simpleName
+        const val SIGN_IN_RESULT_CODE = 1001
     }
 }
--- starter/app/src/main/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragment.kt
+++ starter/app/src/main/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragment.kt
@@ -1,9 +1,13 @@
 package com.udacity.project4.locationreminders.reminderslist
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.*
 import androidx.databinding.DataBindingUtil
+import com.firebase.ui.auth.AuthUI
+import com.google.android.material.snackbar.Snackbar
 import com.udacity.project4.R
+import com.udacity.project4.authentication.AuthenticationActivity
 import com.udacity.project4.base.BaseFragment
 import com.udacity.project4.base.NavigationCommand
 import com.udacity.project4.databinding.FragmentRemindersBinding
@@ -71,7 +75,22 @@ class ReminderListFragment : BaseFragment() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.logout -> {
-//                TODO: add the logout implementation
+                AuthUI.getInstance().signOut(requireContext())
+                    .addOnCompleteListener { it ->
+                        if (it.isSuccessful) {
+                            requireActivity().let {
+                                val intent = Intent(it, AuthenticationActivity::class.java)
+                                startActivity(intent)
+                                it.finish()
+                            }
+                        } else {
+                            Snackbar.make(
+                                requireView(),
+                                getString(R.string.logout_failed),
+                                Snackbar.LENGTH_LONG
+                            ).show()
+                        }
+                    }
             }
         }
         return super.onOptionsItemSelected(item)
